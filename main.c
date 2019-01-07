67e48753b9d1d68d2e38f3b00c176bcc302ed85c
b1065119Hanzo <yu0921251323@gmail.com>
2019-01-07 15:22:18 +0800

    add b

--- main.c
+++ main.c
@@ -4,10 +4,14 @@
 int main()
 {
     int a;
+    double b;
     a=1;
     a=a+10;
+    b=6.999;
     printf("666\n");
 
-    printf("a=%d",a);
+
+    printf("a=%d\n",a);
+    printf("b=%lf",b);
     return 0;
 }
